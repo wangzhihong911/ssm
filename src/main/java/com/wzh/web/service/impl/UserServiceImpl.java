e10216cefbdaf5d1218c6f8beb754e2a0b728776
wangzhihong911 <wangzhihong911@github.com>
2018-07-23 01:00:07 +0800

    设置注解事务  抛出一个运行时异常

--- src/main/java/com/wzh/web/service/impl/UserServiceImpl.java
+++ src/main/java/com/wzh/web/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.wzh.web.dao.UserDAO;
 import com.wzh.web.model.UserDTO;
 import com.wzh.web.service.UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -13,12 +14,15 @@ public class UserServiceImpl implements UserService {
     @Resource
     private UserDAO userDAO;
 
+    //@Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
+    @Transactional(readOnly = true)
     public List<UserDTO> queryList() {
         List<UserDTO> list = null;
         try{
             list = this.userDAO.queryList();
         }catch (Exception ex){
             ex.printStackTrace();
+            throw new RuntimeException();
         }
 
         return list;
